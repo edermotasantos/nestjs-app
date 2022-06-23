6563c66d321b4ba7ad8a381206487ef11a428070
Eder Santos <3derms@gmail.com>
2022-06-22 22:15:08 -0300

    test: testa criação de tarefa

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -5,16 +5,47 @@ import { TasksService } from './tasks.service';
 describe('TasksController', () => {
   let controller: TasksController;
 
+  const mockTasksService = {
+    create: jest.fn((dto) => {
+      return {
+        _id: Date.now(),
+        ...dto,
+      };
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TasksController],
       providers: [TasksService],
-    }).compile();
+    })
+      .overrideProvider(TasksService)
+      .useValue(mockTasksService)
+      .compile();
 
     controller = module.get<TasksController>(TasksController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create a task', () => {
+    expect(
+      controller.create({
+        nome: 'primeira tarefa',
+        data_de_execucao: new Date('2022-06-22'),
+        situacao: 'pendente',
+        prioridade: 'alta',
+        data_de_conclusao: new Date('2022-06-30'),
+      }),
+    ).toEqual({
+      nome: 'primeira tarefa',
+      data_de_execucao: new Date('2022-06-22'),
+      situacao: 'pendente',
+      prioridade: 'alta',
+      data_de_conclusao: new Date('2022-06-30'),
+      _id: expect.any(Number),
+    });
   });
 });
